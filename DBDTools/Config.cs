3a164f6bfff5174cc47f936cba93aa8355bf3758
Neinndall <66384195+Neinndall@users.noreply.github.com>
2024-08-18 00:19:15 +0200

    update

--- DBDTools/Config.cs
+++ DBDTools/Config.cs
@@ -1,8 +1,8 @@
 public static class Config
 {
     // Variables de configuración
-    public const string AppName = "WEMAudioRenamer";
+    public const string AppName = "DBDTools";
     public const string Version = "0.1.0.0";
     public const string Company = "Neinndall";
-    public const string Description = "A tool to rename WEM files based on data in JSON files.";
+    public const string Description = "DBDTools is an application that can extract assets abd rename wem files.";
 }
